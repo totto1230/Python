4084c8f98a65d9c90f7f3c8e00ee222e5822b820
totto <joseph.granados@hotmail.com>
2023-12-26 10:03:27 -0600

    Improvements on the search

--- shoppin.py
+++ shoppin.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
+# Author: Your Name: https://github.com/totto1230
+# This is a script to identify webstores 
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import emoji
 import os
 
-#ref: https://stackoverflow.com/questions/55405590/determining-if-a-website-is-a-web-store-or-not
-
-
 def get_websites():
-    # opening the file in read mode 
-    with open('websites.txt', 'r') as archivo:
+    # opening the file in read mode, replace 'file_name' with the actual name of your file
+    with open('test_webs.txt', 'r') as archivo:
         lineas = archivo.read().splitlines()
     websites = [linea for linea in lineas]
     size=len(websites)
     print(websites)
-    print(f"NUMBERS OF WEBSITES GATHERED: {size}"+ emoji.emojize(':thinking_face:'))
+    print(f"NUMBERS OF WEBSITES GATHERED: {size} "+ emoji.emojize(':thinking_face:'))
     return websites
 
 def get_reaches(all_websites):
     scoped=[]
     not_reach=[]
-    print("REVIEWING THE WEBSITES..." + emoji.emojize(':thinking_face:'))
+    print("REVIEWING THE WEBSITES... " + emoji.emojize(':thinking_face:'))
     for url in all_websites:
         response = os.system("ping -c 1 " + url + " > /dev/null 2>&1")
         if response == 0:
@@ -34,18 +34,34 @@ def check_websites(websites_heal,web_notrec):
     shops = []
     not_reach=web_notrec
     not_shop=[]
+    ##TODO:
+    ##Add more selectors or/and improve the selection method.
     selectors = [
-    'a[href*="cart"]',           # Links with "cart" in href attribute
-    'li[class*="cart"]',         # List items with "cart" in class attribute
-    'a[class="cart_link"]',      # Links with a specific class "cart_link"
-    'button:contains("Add to Cart")',  # Buttons with specific text
-    #'[id*="cart"]',              # Elements with "cart" in the ID attribute
-    'span:contains("Cart")',     # Spans with specific text
-    '.checkout',                 # Elements with class "checkout"
-    'div[data-cart]']            # Divs with a data attribute related to the cart
+    'a[href*="cart"]',                     # Matches links with "cart" in href attribute
+    'a[href*="minicart-link"]',            # Matches links with "minicart-link" in href attribute
+    'li[class*="cart"]',                   # Matches list items with "cart" in class attribute
+    'a[class="cart_link"]',                 # Matches links with a specific class "cart_link"
+    'a[class_="minicart-link"]',            # Matches links with a specific class "minicart-link"
+    'button:contains("Add to Cart")',      # Matches buttons with specific text "Add to Cart"
+    'a[class*="minicart-link"]',            # Matches links with "minicart-link" in class attribute
+    'a[class*="minicart_link"]',            # Matches links with "minicart_link" in class attribute
+    'span:contains("Cart")',                # Matches spans with specific text "Cart"
+    '.checkout',                            # Matches elements with class "checkout"
+    'div[data-cart]',                       # Matches divs with data-cart attribute
+    'button[class*="cart"]',                # Matches buttons with "cart" in class attribute
+    'span[class*="cart"]',                  # Matches spans with "cart" in class attribute
+    'div[class*="cart"]',                   # Matches divs with "cart" in class attribute
+    'input[type="button"][value*="Cart"]', # Matches input buttons with "Cart" in value attribute
+    'a:has(img[src*="cart"])',              # Matches links with an image containing "cart" in src attribute
+    'a[aria-label*="Cart"]',                # Matches links with "Cart" in aria-label attribute
+    'a[aria-label*="Shopping"]',             # Matches links with "Shopping" in aria-label attribute
+    'a.minicart-link',
+    'div.minicart a.minicart-link'  # Matches cart link
+    ] 
     combined_selector = ', '.join(selectors)
     i=1
     options = webdriver.ChromeOptions()
+    ##Preventing chrome from launching GUI
     options.headless = True
     options.add_argument('window-size=1200x600')  # Set a small window size
     options.add_argument('log-level=3')
@@ -76,18 +92,18 @@ def check_websites(websites_heal,web_notrec):
     os.system('clear||cls')
     print("RESULTS: \n")
     if len(shops) > 0:
-        print('\nSHOPS FOUND:')
+        print('\nSHOPS FOUND: ')
         for shop in shops:
-            print(shop + "✓\n")
+            print(shop + " ✓\n")
     else:
-        print("DIDN'T FIND ANYTHING :d")
+        print("DIDN'T FIND ANYTHING :d \n")
     
     for notr in not_reach:
-        print(f"{notr} is not reachable " + "✕\n")
+        print(f"{notr} is not reachable " + " ✕\n")
     print("TOTAL OF WEBSITES NOT REACHABLE: " + str(len(not_reach)) + " \n")       
     
     for noshop in not_shop:
-        print(f"{noshop} is not a shop")
+        print(f"{noshop} is not a shop ")
 
 
 
